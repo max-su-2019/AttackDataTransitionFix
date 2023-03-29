a13003db96e2729b256bd796f30fb259e5b5b183
max-su-2019 <54713650+max-su-2019@users.noreply.github.com>
2023-03-29 22:20:22 +0800

    Update to v0.02

--- src/DataHandler.cpp
+++ src/DataHandler.cpp
@@ -2,19 +2,40 @@
 
 namespace AttackDataTransitionFix
 {
-	const char powerAttackEvent[] = "attackPowerStartInPlace";
-	const char MCO_PowerAttackEvent[] = "MCO_PowerAttackInitiate";
+	//attackDataName: Name of the attack data to delay transition
+	//variableNames: List of the graph variable boolen names that attach to the behavior attack state
+	//reapplyEvent: The animation event name that retrigger the attack data trasition
+	void DelayedAttackDataHandler::InsertDelayedAttackData(const std::string& attackDataName, const std::vector<std::string> variableNames, const std::string& reapplyEvent)
+	{
+		delayedAttackDataMap.emplace(attackDataName, variableNames);
+		reapplyAttackDataMap.emplace(reapplyEvent, attackDataName);
+	}
 
 	DelayedAttackDataHandler::DelayedAttackDataHandler()
 	{
-		delayedAttackDataMap.emplace(powerAttackEvent, std::vector<std::string>{ "IsAttacking" });
-		reapplyAttackDataMap.emplace(MCO_PowerAttackEvent, powerAttackEvent);
+		InsertDelayedAttackData("attackPowerStartInPlace", { "MCO_IsAttacking", "MCO_InSprintAttack" }, "MCO_PowerAttackInitiate");
+		InsertDelayedAttackData("attackPowerStartForwardH2HRightHand", { "MCO_IsAttacking", "MCO_InSprintAttack" }, "MCO_PowerAttackInitiateH2H");
+
+		InsertDelayedAttackData("attackStart", { "MCO_IsPowerAttacking", "MCO_InSprintPowerAttack" }, "MCO_AttackInitiate");
+		InsertDelayedAttackData("AttackStartH2HRight", { "MCO_IsPowerAttacking", "MCO_InSprintPowerAttack" }, "MCO_AttackInitiateH2H");
 	}
 
 	bool DelayedAttackDataHandler::ShouldDelayAttack(RE::Actor* playerRef, const RE::BSFixedString& a_animationEvent)
 	{
-		return delayedAttackDataMap.find(a_animationEvent.c_str()) != delayedAttackDataMap.end() &&
-		       FindAttackData(playerRef, a_animationEvent);
+		if (!FindAttackData(playerRef, a_animationEvent))
+			return false;
+
+		auto pair = delayedAttackDataMap.find(a_animationEvent.c_str());
+		if (pair != delayedAttackDataMap.end()) {
+			for (const auto& varName : pair->second) {
+				bool result = false;
+				if (playerRef->GetGraphVariableBool(varName, result) && result) {
+					return true;
+				}
+			}
+		}
+
+		return false;
 	}
 
 	bool DelayedAttackDataHandler::ReapplyAttackData(RE::Actor* playerRef, const RE::BSFixedString& a_animationEvent)
@@ -26,12 +47,12 @@ namespace AttackDataTransitionFix
 		if (!playerHighData || !playerHighData->attackData)
 			return false;
 
-		auto item = reapplyAttackDataMap.find(a_animationEvent.c_str());
-		if (item != reapplyAttackDataMap.end()) {
-			auto attackData = FindAttackData(playerRef, item->second);
+		auto pair = reapplyAttackDataMap.find(a_animationEvent.c_str());
+		if (pair != reapplyAttackDataMap.end()) {
+			auto attackData = FindAttackData(playerRef, pair->second);
 			if (attackData && playerHighData->attackData.get() != attackData) {
 				playerHighData->attackData.reset(attackData);
-				DEBUG("Reapply Attack Data \"{}\" in event \"{}\"", item->second, item->first);
+				DEBUG("Reapply Attack Data \"{}\" in event \"{}\"", pair->second, pair->first);
 				return true;
 			}
 		}
@@ -48,12 +69,13 @@ namespace AttackDataTransitionFix
 		auto race = playerRef->GetRace();
 		auto attackDataMap = race ? race->attackDataMap.get() : nullptr;
 		if (attackDataMap) {
-			auto item = attackDataMap->attackDataMap.find(a_animationEvent);
-			if (item != attackDataMap->attackDataMap.end() && item->second) {
-				return item->second.get();
+			auto pair = attackDataMap->attackDataMap.find(a_animationEvent);
+			if (pair != attackDataMap->attackDataMap.end() && pair->second) {
+				return pair->second.get();
 			}
 		}
 
 		return nullptr;
 	}
+
 }
--- src/DataHandler.h
+++ src/DataHandler.h
@@ -17,6 +17,8 @@ namespace AttackDataTransitionFix
 		static RE::BGSAttackData* FindAttackData(RE::Actor* playerRef, const RE::BSFixedString& a_animationEvent);
 
 	private:
+		void InsertDelayedAttackData(const std::string& attackDataName, const std::vector<std::string> variableNames, const std::string& reapplyEvent);
+
 		std::map<std::string, const std::vector<std::string>> delayedAttackDataMap;
 		std::map<std::string, const std::string> reapplyAttackDataMap;
 	};
